f0e1d219c93afb386e4893041180310027d4a978
jamesbrobb <james@jamesrobb.com>
2022-02-17 12:47:37 +0000

    refactored contol group type definitions to be more specific

--- src/app/config/controls/controls-config.ts
+++ src/app/config/controls/controls-config.ts
@@ -1,3 +1,65 @@
+import {TypeGuard} from "@jbr/types";
+
+
+export enum ControlTypes {
+  header='header',
+  divider='divider',
+  input='input',
+  select='select',
+  json='json'
+}
+
+export type ControlTypeStrings = keyof typeof ControlTypes
+
+
+export enum InputTypes {
+  text='text',
+  email='email',
+  url='url',
+  num='number',
+  checkbox='checkbox',
+  radio='radio'
+}
+
+
+export type ControlGroup = InputControl | SelectControl | JsonControl | ControlGroupHeader | ControlGroupDivider;
+export type InteractiveControlGroup = Extract<ControlGroup, InteractiveControl>
+
+
+export type LabeledControl = {
+  label: string
+}
+
+export type InteractiveControl = {
+  key: string,
+  value?: unknown
+}
+
+export type InputControl = {
+  controlType: ControlTypes.input,
+  type: InputTypes,
+  min?: number,
+  max?: number
+} & LabeledControl & InteractiveControl
+
+export type SelectControl = {
+  controlType: ControlTypes.select,
+  options?: ControlGroupOption[],
+  optionsId?: string,
+} & LabeledControl & InteractiveControl
+
+export type JsonControl = {
+  controlType: ControlTypes.json,
+} & LabeledControl & InteractiveControl;
+
+export type ControlGroupHeader = {
+  controlType: ControlTypes.header,
+  label: string
+}
+
+export type ControlGroupDivider = {
+  controlType: ControlTypes.divider
+}
 
 
 export type ControlGroupOption = {
@@ -6,14 +68,18 @@ export type ControlGroupOption = {
   label?: string
 }
 
-export type ControlGroup = {
-  controlType: 'input' | 'select' | 'code-mirror' | 'json-editor' | 'divider' | 'header',
-  key: string,
-  label: string,
-  type?: 'text' | 'email' | 'url' | 'number' | 'checkbox' | 'radio',
-  value?: string,
-  options?: ControlGroupOption[],
-  optionsId?: string,
-  min?: number,
-  max?: number
-};
+
+export const isInputControl = controlTypeGuard<InputControl>(ControlTypes.input);
+export const isSelectControl = controlTypeGuard<SelectControl>(ControlTypes.select);
+export const isJsonControl = controlTypeGuard<JsonControl>(ControlTypes.json);
+export const isHeaderControl = controlTypeGuard<ControlGroupHeader>(ControlTypes.header);
+export const isDividerControl = controlTypeGuard<ControlGroupDivider>(ControlTypes.divider);
+
+export const isInteractiveControl: TypeGuard<ControlGroup, InteractiveControlGroup>
+  = (control: ControlGroup): control is InteractiveControlGroup => 'key' in control;
+
+
+
+function controlTypeGuard<C extends ControlGroup>(type: ControlTypeStrings): TypeGuard<ControlGroup, C> {
+  return (control: ControlGroup): control is C => control.controlType === type
+}
